d96f46f1e62a54dd7e49da54f05c290ca55b4923
snowflakekiller <karuna.skj@gmail.com>
2016-06-13 16:53:10 +0800

    add more scoping to posts project

--- app.js
+++ app.js
@@ -3,4 +3,17 @@ angular.module('flapperNews', [])
 '$scope',
 function($scope){
   $scope.test = 'Hello world!';
+
+  $scope.posts = [
+    {title: '1', upvotes: 5},
+    {title: '2', upvotes: 2},
+    {title: '3', upvotes: 15},
+    {title: '4', upvotes: 9},
+    {title: '5', upvotes: 4}
+  ];
+
+  $scope.addPost = function(){
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
+  };
 }]);
